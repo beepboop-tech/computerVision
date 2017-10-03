65cf7649969da48cc295a5508da4e62c86ea58bd
Albie Baker-Smith <ab15085@it025966.users.bris.ac.uk>
2017-10-03 10:08:33 +0100

    task 1 can run on different images

--- labs/lab2/task1.cpp
+++ labs/lab2/task1.cpp
@@ -3,30 +3,71 @@
 #include <opencv/highgui.h>   //adjust import locations
 #include <opencv/cxcore.h>    //depending on your machine setup
 #include <stdlib.h>
+#include <iostream>
 
 using namespace cv;
+using namespace std;
 
+void mandrill0(Mat *image) {
+
+}
+
+void mandrill1(Mat *image) {
+
+}
 
 // Hypothisises:
 //      mandrill2 is the inverse
-
-int main(int argc, char *argv[]) {
-    // Read in the image
-    Mat image;
-    image = imread("images/mandrill2.jpg", CV_LOAD_IMAGE_COLOR);
-
+void mandrill2(Mat *image) {
     Vec3b v = Vec3b(255, 255, 255);
 
-
-
-
     // Iterate through each pixel
-    for (int y=0; y<image.rows; y++){
-        for (int x=0; x<image.rows; x++){
+    for (int y=0; y<image->rows; y++){
+        for (int x=0; x<image->rows; x++){
             // Do the thresholding
-            Vec3b pixel = image.at<Vec3b>(y,x);
-            image.at<Vec3b>(y, x) = v - pixel;
+            Vec3b pixel = image->at<Vec3b>(y,x);
+            image->at<Vec3b>(y, x) = v - pixel;
         }
+    }
+}
+
+void mandrill3(Mat *image) {
+
+}
+
+int main(int argc, char *argv[]) {
+    if (argc != 2) {
+        cout << "Incorrect number of arguments. Expected the image index.\n";
+        return 0;
+    }
+
+    int imageIndex = atoi(argv[1]);
+
+    // Read in the image
+    //stringstream fileName = "images/mandrill" << imageIndex << ".jpg";
+
+    Mat image;
+
+    switch (imageIndex) {
+        case 0:
+            image = imread("images/mandrill0.jpg", CV_LOAD_IMAGE_COLOR);
+            mandrill0(&image);
+            break;
+        case 1:
+            image = imread("images/mandrill1.jpg", CV_LOAD_IMAGE_COLOR);
+            mandrill1(&image);
+            break;
+        case 2:
+            image = imread("images/mandrill2.jpg", CV_LOAD_IMAGE_COLOR);
+            mandrill2(&image);
+            break;
+        case 3:
+            image = imread("images/mandrill3.jpg", CV_LOAD_IMAGE_COLOR);
+            mandrill3(&image);
+            break;
+        default:
+            cout << "Incorrect image index. Must be in the range 0-3\n";
+            return 0;
     }
 
     // Write the output
