a102b1b985602ec8354acfb7cb76d1482ff2857b
Albie Baker-Smith <ab15085@it025966.users.bris.ac.uk>
2017-10-03 10:49:15 +0100

    task2 part 1 done

--- labs/lab2/task1.cpp
+++ labs/lab2/task1.cpp
@@ -8,30 +8,45 @@
 using namespace cv;
 using namespace std;
 
-void mandrill0(Mat *image) {
+void mandrill0(Mat *image,  Mat *output) {
 
 }
 
-void mandrill1(Mat *image) {
+// Shifts the red channel of the image a certain amount.
+void mandrill1(Mat *image, Mat *output) {
+    int shift = 30; // pixels
 
-}
-
-// Hypothisises:
-//      mandrill2 is the inverse
-void mandrill2(Mat *image) {
-    Vec3b v = Vec3b(255, 255, 255);
-
-    // Iterate through each pixel
     for (int y=0; y<image->rows; y++){
-        for (int x=0; x<image->rows; x++){
-            // Do the thresholding
-            Vec3b pixel = image->at<Vec3b>(y,x);
-            image->at<Vec3b>(y, x) = v - pixel;
+        for (int x=0; x<image->cols; x++){
+            int destX = (x + shift) % image->cols;
+            int destY = (y + shift) % image->rows;
+
+            // Remove some red (BGR) from the current pixel.
+            uchar red   = image->at<Vec3b>(y, x)[2];
+            uchar green = image->at<Vec3b>(destY, destX)[1];
+            uchar blue  = image->at<Vec3b>(destY, destX)[0];
+
+            output->at<Vec3b>(destY, destX) = Vec3b(blue, green, red);
         }
     }
 }
 
-void mandrill3(Mat *image) {
+// Hypothisises:
+//      mandrill2 is the inverse
+void mandrill2(Mat *image, Mat *output) {
+    Vec3b v = Vec3b(255, 255, 255);
+
+    // Iterate through each pixel
+    for (int y=0; y<image->rows; y++){
+        for (int x=0; x<image->cols; x++){
+            // Do the thresholding
+            Vec3b pixel = image->at<Vec3b>(y,x);
+            output->at<Vec3b>(y, x) = v - pixel;
+        }
+    }
+}
+
+void mandrill3(Mat *image, Mat *output) {
 
 }
 
@@ -46,24 +61,28 @@ int main(int argc, char *argv[]) {
     // Read in the image
     //stringstream fileName = "images/mandrill" << imageIndex << ".jpg";
 
-    Mat image;
+    Mat image, newImage;
 
     switch (imageIndex) {
         case 0:
             image = imread("images/mandrill0.jpg", CV_LOAD_IMAGE_COLOR);
-            mandrill0(&image);
+            newImage = Mat(image.rows, image.cols, CV_8UC3);
+            mandrill0(&image, &newImage);
             break;
         case 1:
             image = imread("images/mandrill1.jpg", CV_LOAD_IMAGE_COLOR);
-            mandrill1(&image);
+            newImage = Mat(image.rows, image.cols, CV_8UC3);
+            mandrill1(&image, &newImage);
             break;
         case 2:
             image = imread("images/mandrill2.jpg", CV_LOAD_IMAGE_COLOR);
-            mandrill2(&image);
+            newImage = Mat(image.rows, image.cols, CV_8UC3);
+            mandrill2(&image, &newImage);
             break;
         case 3:
             image = imread("images/mandrill3.jpg", CV_LOAD_IMAGE_COLOR);
-            mandrill3(&image);
+            newImage = Mat(image.rows, image.cols, CV_8UC3);
+            mandrill3(&image, &newImage);
             break;
         default:
             cout << "Incorrect image index. Must be in the range 0-3\n";
@@ -72,7 +91,7 @@ int main(int argc, char *argv[]) {
 
     // Write the output
     namedWindow("Display window", WINDOW_AUTOSIZE); // Create a window for display.
-    imshow("Display window", image);                   // Show our image inside it.
+    imshow("Display window", newImage);             // Show our image inside it.
     waitKey(0);
 
     return 0;
